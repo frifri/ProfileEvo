b811d6f97a7f8595bbc052b55a184a86a46bde13
Francis Genet <58907@supinfo.com>
2012-02-07 14:25:48 +0100

    License + README

--- FenetrePrincipale.cpp
+++ FenetrePrincipale.cpp
@@ -1,11 +1,23 @@
 /*
- *  FenetrePrincipale.cpp
- *  ProfileSaver
- *
- *  Created by Francis Genet on 26/12/09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
+* Copyright 2009 - 2012 Francis Genet.
+*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 
 #include "FenetrePrincipale.h"
 #include "FenetreProgression.h"
@@ -154,7 +166,7 @@ void FenetrePrincipale::recProfilIm()
 	/* Pour ajouter un logiciel, ajouter juste cette partie
 	 else if(m_chxLogicielIm->currentText() == "Nom_Dans_La_Liste")
 	 {
-	 FenetreProgression *fenProgression = naew FenetreProgression("Nom_Dans_La_Liste", this);
+	 FenetreProgression *fenProgression = new FenetreProgression("Nom_Dans_La_Liste", this);
 	 fenProgression->exec();
 	 }
 	 */
@@ -181,7 +193,7 @@ void FenetrePrincipale::recProfilEx()
 	/* Pour ajouter un logiciel, ajouter juste cette partie
 	else if(m_chxLogicielEx->currentText() == "Nom_Dans_La_Liste")
 	{
-		FenetreProgression *fenProgression = naew FenetreProgression("Nom_Dans_La_Liste", this);
+		FenetreProgression *fenProgression = new FenetreProgression("Nom_Dans_La_Liste", this);
 		fenProgression->exec();
 	}
 	*/
